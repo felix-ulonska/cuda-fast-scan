c621710f4368e8bc7e8214fda962d2fffe487524
Felix Ulonska <git@webfoo.de>
2022-05-06 18:38:00 +0200

    nix: changed to fhs shell

--- shell.nix
+++ shell.nix
@@ -10,21 +10,44 @@ let
      #rev = "c777cdf5c564015d5f63b09cc93bef4178b19b01";
    }) {};
 in 
-pkgs.stdenv.mkDerivation {
+let 
+  pkgs =
+    import (fetchTarball
+      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz) {};
+  fhs = pkgs.buildFHSUserEnv {
+    name = "cuda-env";
+    targetPkgs = pkgs: with pkgs; [ 
+      git
+      gitRepo
+      gnupg
+      autoconf
+      curl
+      procps
+      gnumake
+      utillinux
+      m4
+      gperf
+      unzip
+      cudatoolkit
+      linuxPackages.nvidia_x11
+      libGLU libGL
+      xorg.libXi xorg.libXmu freeglut
+      xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
+      ncurses5
+      stdenv.cc
+      binutils
+    ];
+    multiPkgs = pkgs: with pkgs; [ zlib ];
+    runScript = "bash";
+    profile = ''
+       export CUDA_PATH=${pkgs.cudatoolkit}
+       # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
+       export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+       export EXTRA_CCFLAGS="-I/usr/include"
+     '';
+  };
+in pkgs.stdenv.mkDerivation {
    name = "cuda-env-shell";
-   buildInputs = with pkgs; [
-     git gitRepo gnupg autoconf curl
-     procps gnumake utillinux m4 gperf unzip
-     cudatoolkit linuxPackages.nvidia_x11
-     libGLU libGL
-     xorg.libXi xorg.libXmu freeglut
-     xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
-     ncurses5 stdenv.cc binutils
-   ];
-   shellHook = ''
-      export CUDA_PATH=${pkgs.cudatoolkit}
-      # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
-      export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-      export EXTRA_CCFLAGS="-I/usr/include"
-   '';          
+   nativeBuildInputs = [ fhs ];
+   shellHook = "exec cuda-env";
 }
