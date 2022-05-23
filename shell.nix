284545e966cea35573e9a8b09348099773bd135d
Felix Ulonska <git@webfoo.de>
2022-05-23 18:18:48 +0200

    fix(shell): removed fsh

--- shell.nix
+++ shell.nix
@@ -3,13 +3,13 @@ let
    import (builtins.fetchGit {
      name = "nixos-unstable-2022-05-06";
      url = "https://github.com/nixos/nixpkgs/";
-     ref = "refs/heads/nixos-unstable";
+     rev = "4b2827e6a180274a4df35f0754cc6353ca853998";
    }) {};
-in 
-let 
-  fhs = pkgs.buildFHSUserEnv {
-    name = "cuda-env";
-    targetPkgs = pkgs: with pkgs; [ 
+in
+  pkgs.stdenv.mkDerivation {
+     name = "cuda-env-shell";
+     nativeBuildInputs = with pkgs; [ 
+       #fhs
       git
       gitRepo
       gnupg
@@ -35,28 +35,13 @@ let
       stdenv.cc
       binutils
       gdb
-    ];
-    multiPkgs = pkgs: with pkgs; [ zlib ];
-    runScript = "bash";
-    profile = ''
+     ];
+     shellHook = ''
        export CUDA_PATH=${pkgs.cudatoolkit}
        # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
        export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
        export EXTRA_CCFLAGS="-I/usr/include"
-
+     #exec cuda-env
      '';
-  };
-in 
-  pkgs.stdenv.mkDerivation {
-    
-     name = "cuda-env-shell";
-     nativeBuildInputs = with pkgs; [ 
-       qt5.qtbase
-       cudaPackages.nsight_systems
-       fhs ];
-       shellHook = ''
-       # for nsight_compute https://discourse.nixos.org/t/python-qt-qpa-plugin-could-not-find-xcb/8862
-       export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins"
-       exec cuda-env'';
   }
 
