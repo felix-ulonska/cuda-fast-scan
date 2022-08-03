8f996c82dbda2e32a013fc0e1a6547e9090152e1
Felix Ulonska <git@webfoo.de>
2022-08-03 23:58:07 +0200

    benchmark version

--- baseline/params.cuh
+++ baseline/params.cuh
@@ -4,8 +4,8 @@
 
 #define ITEMS_PER_THREAD 2
 #define THREADS_PER_BLOCK 512
-#define AMOUNT_BLOCKS 1235
-#define CSV_OUTPUT_PATH "Benchmarking/window_1/1235.csv"
-
+#define AMOUNT_BLOCKS 596
+#define CSV_OUTPUT_PATH "Benchmarking/window_50/596.csv"
+#define WINDOW 50
 #endif
     
