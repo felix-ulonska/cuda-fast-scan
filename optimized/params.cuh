20bdea63b48562767812351176b36acebf46b6e6
Felix Ulonska <git@webfoo.de>
2022-08-20 16:04:46 +0200

    fromatted

--- optimized/params.cuh
+++ optimized/params.cuh
@@ -5,7 +5,7 @@
 #define ITEMS_PER_THREAD 2
 #define THREADS_PER_BLOCK 512
 #define AMOUNT_BLOCKS 996
-#define CSV_OUTPUT_PATH "Benchmarking/window_1/996.csv"
+#define CSV_OUTPUT_PATH "Benchmarking/bad_window_1/996.csv"
 #define WINDOW 1
 #endif
     
